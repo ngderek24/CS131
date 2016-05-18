629b0739c573c0e986a363097a0c52a11415c6a0
Derek Nguyen <tincan24@gmail.com>
2016-05-18 13:07:07 -0700

    Added more test cases

--- hw4.java
+++ hw4.java
@@ -181,7 +181,6 @@ class CalcTest {
         // extra tests
         exp = new BinOp(new Num(2.0), Op.DIVIDE, new BinOp(new BinOp(new Num(3.0), Op.MINUS, new Num(1.0)), Op.TIMES, new Num(1.0)));
         assert(exp.eval() == 1.0);
-
         List<Instr> il = new LinkedList<Instr>();
         il.add(new Push(2.0));
         il.add(new Push(3.0));
@@ -191,6 +190,65 @@ class CalcTest {
         il.add(new Calculate(Op.TIMES));
         il.add(new Calculate(Op.DIVIDE));
         assert(exp.compile().equals(il));
+        Instrs i = new Instrs(il);
+        assert(i.execute() == 1.0);
+
+        //
+        exp = new BinOp(new BinOp(new Num(3.0), Op.TIMES, new Num(2.0)), Op.MINUS, new BinOp(new Num(4.0), Op.DIVIDE, new Num(1.0)));
+        assert(exp.eval() == 2.0);
+        List<Instr> a = new LinkedList<Instr>();
+        a.add(new Push(3.0));
+        a.add(new Push(2.0));
+        a.add(new Calculate(Op.TIMES));
+        a.add(new Push(4.0));
+        a.add(new Push(1.0));
+        a.add(new Calculate(Op.DIVIDE));
+        a.add(new Calculate(Op.MINUS));
+        assert(exp.compile().equals(a));
+        Instrs i2 = new Instrs(a);
+        assert(i2.execute() == 2.0);
+
+        //
+        exp = new BinOp(new BinOp(new Num(6.0), Op.DIVIDE, new Num(4.0)), Op.MINUS, new Num(3.0));
+        assert(exp.eval() == -1.5);
+        List <Instr> b = new LinkedList<Instr>();
+        b.add(new Push(6.0));
+        b.add(new Push(4.0));
+        b.add(new Calculate(Op.DIVIDE));
+        b.add(new Push(3.0));
+        b.add(new Calculate(Op.MINUS));
+        assert(exp.compile().equals(b));
+        Instrs i3 = new Instrs(b);
+        assert(i3.execute() == -1.5);
+
+        // Jason's extra tests 
+        exp = new BinOp(new BinOp(new Num(3.0), Op.PLUS, new Num(2.0)), Op.TIMES, new BinOp(new Num(1.0), Op.MINUS, new Num(3.0)));
+        assert(exp.eval() == -10.0);
+        List <Instr> jl2 = new LinkedList<Instr>();
+        jl2.add(new Push(3.0));
+        jl2.add(new Push(2.0));
+        jl2.add(new Calculate(Op.PLUS));
+        jl2.add(new Push(1.0));
+        jl2.add(new Push(3.0));
+        jl2.add(new Calculate(Op.MINUS));
+        jl2.add(new Calculate(Op.TIMES));
+        assert(exp.compile().equals(jl2));
+        Instrs ji2 = new Instrs(jl2);
+        assert(ji2.execute() == -10.0);
+        // 
+        exp = new BinOp(new BinOp(new Num(3.0), Op.TIMES, new Num(0.0)), Op.MINUS, new BinOp(new Num(1.0), Op.MINUS, new Num(3.0)));
+        assert(exp.eval() == 2.0);
+        List <Instr> jl3 = new LinkedList<Instr>();
+        jl3.add(new Push(3.0));
+        jl3.add(new Push(0.0));
+        jl3.add(new Calculate(Op.TIMES));
+        jl3.add(new Push(1.0));
+        jl3.add(new Push(3.0));
+        jl3.add(new Calculate(Op.MINUS));
+        jl3.add(new Calculate(Op.MINUS));
+        assert(exp.compile().equals(jl3));
+        Instrs ji3 = new Instrs(jl3);
+        assert(ji3.execute() == 2.0);
     }
 }
 
@@ -307,22 +365,37 @@ class StringSetTest {
     public static void main(String[] args) {
         // tests for Problem 2A
         StringSet set = new ListStringSet();
+        assert(set.size() == 0);
         set.add("hi");
         assert(set.size() == 1);
         assert(set.contains("bye") == false);
-        
         set.add("hey");
         assert(set.size() == 2);
         assert(set.contains("hey") == true);
-
         set.add("hi");
         assert(set.size() == 2);
-
         set.add("sup");
         assert(set.size() == 3);
         assert(set.contains("sup") == true);
-
+        set.add("");
+        assert(set.size() == 4);
         set.print();
+
+        // Jason's tests for Sets
+        StringSet set1 = new ListStringSet();
+        assert(set1.size() == 0);
+        set1.add("hello");
+        set1.add("bye");
+        set1.add("choco");
+        set1.add("");
+        set1.add("bye");
+        assert(set1.size() == 4);
+        assert(set1.contains("hello"));
+        assert(set1.contains("bye"));
+        assert(set1.contains("choco"));
+        assert(set1.contains(""));
+        assert(!set1.contains("duh"));
+        set1.print();
     }
 }
 
@@ -440,6 +513,7 @@ class Element<T> implements Node<T> {
 class SetTest {
     public static void main(String[] args) {
         // tests for Problem 2B
+        // ascending integers
         Set<Integer> set = new ListSet<Integer>((i1, i2) -> i1.compareTo(i2));
         set.add(6);
         assert(set.size() == 1);
@@ -458,6 +532,7 @@ class SetTest {
 
         set.print();
 
+        // reverse alphabetical strings
         Set<String> set2 = new ListSet<String>((s1, s2) -> s2.compareTo(s1));
         set2.add("hi");
         assert(set2.size() == 1);
@@ -475,5 +550,55 @@ class SetTest {
         assert(set2.contains("sup") == true);
 
         set2.print();
+        
+        // decreasing string length
+        Set<String> set3 = new ListSet<String>(new Comparator<String>() {
+                                                public int compare(String s1, String s2) {
+                                                    return s2.length() - s1.length();
+                                                }
+        } );
+        set3.add("a");
+        set3.add("ab");
+        assert(set3.size() == 2);
+        set3.add("a");
+        set3.add("abc");
+        assert(set3.contains("abc") == true);
+        set3.add("bc");
+        set3.print();
+
+        // Jason's
+        Set<String> set4 = new ListSet<String>((String s1, String s2) -> s1.compareTo(s2));
+        assert(set4.size() == 0);
+        set4.add("hello");
+        set4.add("bye");
+        set4.add("choco");
+        set4.add("");
+        set4.add("bye");
+        assert(set4.size() == 4);
+        assert(set4.contains("hello"));
+        assert(set4.contains("bye"));
+        assert(set4.contains("choco"));
+        assert(set4.contains(""));
+        assert(!set4.contains("duh"));
+        set4.print();
+
+        Set<Integer> set5 = new ListSet<Integer>((Integer i1, Integer i2) -> i1 - i2);
+        assert(set5.size() == 0);
+        set5.add(3);
+        set5.add(5);
+        set5.add(-1);
+        set5.add(0);
+        set5.add(6);
+        set5.add(5);
+        set5.add(6);
+        set5.add(-1);
+        assert(set5.size() == 5);
+        assert(set5.contains(3));
+        assert(set5.contains(5));
+        assert(set5.contains(0));
+        assert(set5.contains(-1));
+        assert(set5.contains(6));
+        assert(!set5.contains(7)); 
+        set5.print();
     }
 }
